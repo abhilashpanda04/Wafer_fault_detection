68e78b118f752fa53577e7ae88f7be6d413644c3
Abhilash kumar Panda <abhilashk.isme1517u@gmail.com>
2021-11-03 16:44:15 +0530

    changes made to app.py  tox.ini and ci-cd yaml

--- app.py
+++ app.py
@@ -64,8 +64,8 @@ def index():
 
 
 
-if __name__=="__main__":
-    app.run(host="127.0.0.1",port=5000,debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
 
 
